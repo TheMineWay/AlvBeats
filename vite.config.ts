85be185c1911af99b74bda78d89d049224bad276
Joel Campos Oliva <joelcamposoliva9000@gmail.com>
2025-04-30 19:31:34 +0200

    fix: :wrench: define aliases in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -16,6 +16,11 @@ export default defineConfig({
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
+      "@components": path.resolve(__dirname, "./src/components"),
+      "@constants": path.resolve(__dirname, "./src/constants"),
+      "@assets": path.resolve(__dirname, "./src/assets"),
+      "@utils": path.resolve(__dirname, "./src/utils"),
+      "@i18n": path.resolve(__dirname, "./src/i18n"),
     },
   },
 });
