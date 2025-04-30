c848b270536fc840e126196ea884dd5297224d22
Joel Campos Oliva <joelcamposoliva9000@gmail.com>
2025-04-30 22:47:17 +0200

    fix: :wrench: update path aliases in tsconfig and vite config to include shared and features directories

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
+import tailwindcss from "@tailwindcss/vite";
+import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { defineConfig } from "vite";
-import tailwindcss from "@tailwindcss/vite";
-import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 import { VitePWA } from "vite-plugin-pwa";
 
 const ICONS = [
@@ -144,9 +144,9 @@ export default defineConfig({
       "@components": path.resolve(__dirname, "./src/components"),
       "@constants": path.resolve(__dirname, "./src/constants"),
       "@assets": path.resolve(__dirname, "./src/assets"),
-      "@utils": path.resolve(__dirname, "./src/utils"),
       "@i18n": path.resolve(__dirname, "./src/i18n"),
-      "@types": path.resolve(__dirname, "./src/types"),
+      "@shared": path.resolve(__dirname, "./src/shared"),
+      "@features": path.resolve(__dirname, "./src/features"),
     },
   },
   server: {
