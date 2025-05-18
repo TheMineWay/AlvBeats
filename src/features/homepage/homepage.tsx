fd5c513a6c59919e2a7f2ba2beb7fed4bb94b6a5
Joel Campos Oliva <joelcamposoliva9000@gmail.com>
2025-05-18 21:12:57 +0200

    feat: add song editor button and badge to navigation, update translations

--- src/features/homepage/homepage.tsx
+++ src/features/homepage/homepage.tsx
@@ -1,11 +1,7 @@
 import { SongSearch } from "@/features/songs/song-search/song-search";
 import Container from "@components/layout/container";
-import { Badge } from "@components/ui/badge";
-import { Button } from "@components/ui/button";
-import { Card, CardContent } from "@components/ui/card";
 import { Footer } from "@f-homepage/footer/footer";
 import { Welcome } from "@f-homepage/welcome/welcome";
-import { useTranslation } from "@i18n/use-translation";
 
 export const Homepage: FC = () => {
   return (
@@ -15,32 +11,9 @@ export const Homepage: FC = () => {
         <Welcome />
       </div>
       <Container className="grow flex flex-col gap-4">
-        <Actions />
         <SongSearch />
       </Container>
       <Footer />
     </div>
-  );
-};
-
-/* Internal */
-
-const Actions: FC = () => {
-  const { t } = useTranslation("homepage");
-  const { t: commonT } = useTranslation("common");
-
-  return (
-    <Card>
-      <CardContent className="flex flex-col md:flex-row gap-x-2 gap-y-4">
-        <div className="relative inline-block">
-          <Button size="lg" variant="outline" disabled>
-            {t().main.actions["song-editor"].Title}
-          </Button>
-          <Badge className="absolute -top-2 -right-2">
-            {commonT().sentences["Coming-soon"]}
-          </Badge>
-        </div>
-      </CardContent>
-    </Card>
   );
 };
