3eb699635195805c5b5deef7b12766bbd8a68528
Joel Campos Oliva <joelcamposoliva9000@gmail.com>
2025-06-09 12:47:12 +0200

    feat: card styling

--- src/components/songs/list/song-list.tsx
+++ src/components/songs/list/song-list.tsx
@@ -1,4 +1,5 @@
 import { StoredSong } from "@/models/song/stored-song.model";
+import defaultSongCover from "@assets/song/default-song-cover.png";
 
 type Props = {
   songs: StoredSong[];
@@ -6,18 +7,18 @@ type Props = {
 
 export const SongList: FC<Props> = ({ songs }) => {
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 gap-2">
+    <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-2 h-18">
       {songs.map((song) => (
         <div
           key={song.id}
-          className="p-2 border rounded hover:bg-gray-100 flex gap-2 cursor-pointer transition-colors"
+          className="border rounded hover:bg-gray-100 flex gap-2 cursor-pointer transition-colors"
         >
           <img
-            className="w-12"
+            className="h-18 w-18 rounded-l object-cover"
             alt="cover"
-            src={song.metadata.coverUrl ?? ""}
+            src={song.metadata.coverUrl ?? defaultSongCover}
           />
-          <div className="h-full flex flex-col">
+          <div className="h-full flex flex-col justify-center pl-2">
             <h3 className="text-lg font-semibold">{song.metadata.name}</h3>
             <p className="text-sm text-gray-600">{song.metadata.artist}</p>
           </div>
