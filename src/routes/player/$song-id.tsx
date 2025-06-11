b7c76ed0543d1648ae41b2dcd4a214c6220f90fa
Joel Campos Oliva <joelcamposoliva9000@gmail.com>
2025-06-11 21:53:49 +0200

    feat: song page

--- src/routes/player/$song-id.tsx
+++ src/routes/player/$song-id.tsx
@@ -1,6 +1,9 @@
 import { Song, SONG_SCHEMA } from "@/shared/schemas/song/song.schema";
+import { Button } from "@components/ui/button";
 import { LOCAL_STORAGE_CONNECTOR_KEY } from "@constants/storage/storage-services.constant";
-import { createFileRoute, useParams } from "@tanstack/react-router";
+import { getStoredSongId } from "@features/song/utils/stored/get-stored-song-id";
+import { useTranslation } from "@i18n/use-translation";
+import { createFileRoute, Link, useParams } from "@tanstack/react-router";
 import {
   LocalStorageConnector,
   WebWarehouse,
@@ -11,6 +14,9 @@ export const Route = createFileRoute("/player/$song-id")({
 });
 
 function RouteComponent() {
+  const { t } = useTranslation("song-player");
+  const { t: commonT } = useTranslation("common");
+
   const { "song-id": songId } = useParams({
     from: "/player/$song-id",
   });
@@ -18,10 +24,19 @@ function RouteComponent() {
   const lsConnector: LocalStorageConnector = WebWarehouse.getConnector(
     LOCAL_STORAGE_CONNECTOR_KEY
   );
-  const song = lsConnector.get<Song>(songId, SONG_SCHEMA);
+  const song = lsConnector.get<Song>(getStoredSongId(songId), SONG_SCHEMA);
 
   if (!song) {
-    return <div>Song not found</div>;
+    return (
+      <div className="w-full h-full flex justify-center items-center">
+        <div className="flex flex-col items-center gap-4">
+          <h1 className="text-xl">{t().states["not-found"].Title}</h1>
+          <Link to="/">
+            <Button>{commonT().sentences["Back-home"]}</Button>
+          </Link>
+        </div>
+      </div>
+    );
   }
 
   return <div>{song?.metadata.name}</div>;
