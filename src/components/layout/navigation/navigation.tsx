10f4f85a7a9a13a34871161cae037bef7538f722
Joel Campos Oliva <joelcamposoliva9000@gmail.com>
2025-06-15 13:26:00 +0200

    feat: new nav

--- src/components/layout/navigation/navigation.tsx
+++ src/components/layout/navigation/navigation.tsx
@@ -2,7 +2,16 @@ import { DarkModeSwitch } from "@components/layout/navigation/actions/dark-mode-
 import { ThemeSelector } from "@components/layout/navigation/actions/theme-selector";
 import { Badge } from "@components/ui/badge";
 import { Button } from "@components/ui/button";
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuItem,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+} from "@components/ui/navigation-menu";
 import { useTranslation } from "@i18n/use-translation";
+import { Link } from "@tanstack/react-router";
+import { Home, Menu } from "lucide-react";
 
 export const Navigation: FC = () => {
   const { t: commonT } = useTranslation("common");
@@ -10,16 +19,28 @@ export const Navigation: FC = () => {
 
   return (
     <div className="h-full flex items-center justify-between p-2 bg-background/70 backdrop-blur-md">
-      <div>
-        <div className="relative inline-block">
-          <Button variant="outline" disabled>
-            {t().navigation.actions["song-editor"].Title}
-          </Button>
-          <Badge className="absolute -top-2 -right-2">
-            {commonT().sentences["Coming-soon"]}
-          </Badge>
-        </div>
-      </div>
+      <NavigationMenu>
+        <NavigationMenuList>
+          <NavigationMenuItem>
+            <NavigationMenuTrigger>
+              <Menu />
+            </NavigationMenuTrigger>
+            <NavigationMenuContent>
+              <div className="relative inline-block">
+                <Button variant="outline" disabled>
+                  {t().navigation.actions["song-editor"].Title}
+                </Button>
+                <Badge className="absolute -top-2 -right-2">
+                  {commonT().sentences["Coming-soon"]}
+                </Badge>
+              </div>
+            </NavigationMenuContent>
+          </NavigationMenuItem>
+          <Link to="/">
+            <Home />
+          </Link>
+        </NavigationMenuList>
+      </NavigationMenu>
       <div className="flex items-center gap-4">
         <DarkModeSwitch />
         <ThemeSelector />
