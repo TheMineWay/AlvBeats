e2651f4066274dde9a160ec9f400a05c543a335e
Joel Campos Oliva <joelcamposoliva9000@gmail.com>
2025-06-16 19:26:15 +0200

    feat: by is part of lyric

--- src/shared/schemas/song/lyrics/lyric.schema.ts
+++ src/shared/schemas/song/lyrics/lyric.schema.ts
@@ -2,13 +2,13 @@ import { z } from "zod";
 
 const LYRIC_SEGMENT = z.object({
   text: z.string().nonempty().max(128),
-  by: z.string().nullable().default(null),
 });
 
 export const LYRIC = z.object({
   segments: z.array(LYRIC_SEGMENT).max(64).min(1).nonempty(),
   startTime: z.number().int().nonnegative(), // In milliseconds
   endTime: z.number().int().nullable().default(null), // In milliseconds, null if the lyric is ongoing
+  by: z.string().nullable().default(null),
 });
 
 export type Lyric = z.infer<typeof LYRIC>;
